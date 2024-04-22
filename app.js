53f6d10dbbbb164a5165f031264ec1f5d615a4af
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 13:55:46 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // write code here
+ <script>
+        for (let i=1; i<=9; i++) {
+            console.log(i)
+            if (i===6) 
+                break
+        }
+            console.log("End")
+
+            //for (let i=1; i<=9; i++) {
+              //  if (i===6) 
+             //   break
+           // }
+           // console.log(i)
+            //}
+           // console.log("End")
+        
+    </script>
